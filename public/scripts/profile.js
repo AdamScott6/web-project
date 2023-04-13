fb66c6f4686671b64848128edd11ec8ee3341014
SaraBZL <sara.aziz.bhatti@gmail.com>
2023-04-13 18:33:32 -0400

    edits

--- public/scripts/profile.js
+++ public/scripts/profile.js
@@ -1,19 +1,31 @@
-function changeimage(event) {
-    var image = new FileReader();
-    image.onload = function(){
-      var output_image = document.getElementById('profile_pic');
-      output_image.src = image.result;
-    }
-    image.readAsDataURL(event.target.files[0]);
-  }
+// to change image for banner when user clicks the banner
+document.getElementById("banner_image").addEventListener("click", function() {
+  document.getElementById("banner_input").click();
+});
 
+document.getElementById("banner_input").addEventListener("change", function() {
+  const banner = this.files[0];
+  const fileReader = new FileReader();
+  
+  fileReader.onload = function(event) {
+    document.getElementById("banner_image").src = event.target.result;
+  };
 
+  fileReader.readAsDataURL(banner);
+});
 
-function changeBannerImage(event) {
-    var banner = event.target.files[0];
-    var fileReader = new FileReader();
-    fileReader.onload = function(event) {
-        document.getElementById("banner_image").src = event.target.result;
-    };
-    fileReader.readAsDataURL(banner);
-}
+//to change image for profile pic when user clicks the image
+document.getElementById("profile_pic").addEventListener("click", function() {
+  document.getElementById("profile_input").click();
+});
+
+document.getElementById("profile_input").addEventListener("change", function() {
+  const profile = this.files[0];
+  const fileReader = new FileReader();
+  
+  fileReader.onload = function(event) {
+    document.getElementById("profile_pic").src = event.target.result;
+  };
+
+  fileReader.readAsDataURL(profile);
+});
