2036cd5b2afadc920466745d71c7f7cd62b42c73
SaraBZL <sara.aziz.bhatti@gmail.com>
2023-04-16 14:46:15 -0400

    update
    new way to populate profile details on page

--- public/scripts/profile.js
+++ public/scripts/profile.js
@@ -1,26 +1,47 @@
 // GET call for current user (hardcoded user)
-fetch('/current-user')
-  .then(function(response) {
-    if (!response.ok) {
-      throw new Error('Network response was not ok');
-    }
-    return response.json();
-  })
-  .then(function(data) {
-    console.log("John Retrieved")
-    console.log(data);
-    setUserValues(data);
-  })
-  .catch(function(error) {
-    console.error('There was a problem with the fetch operation:', error);
-  });
+// fetch('/current-user')
+//   .then(function(response) {
+//     if (!response.ok) {
+//       throw new Error('Network response was not ok');
+//     }
+//     return response.json();
+//   })
+//   .then(function(data) {
+//     console.log("John Retrieved")
+//     console.log(data);
+//     setUserValues(data);
+//   })
+//   .catch(function(error) {
+//     console.error('There was a problem with the fetch operation:', error);
+//   });
 
-function setUserValues(userData) {
-  // Set html with currentUser data
-  document.getElementById("user_about_me").innerText = userData.aboutMe;
-  document.getElementById("user_handle").innerText = ("@" + userData.username);
-  document.getElementById("user_name").innerText = userData.fullName;
-}
+// function setUserValues(userData) {
+//   // Set html with currentUser data
+//   document.getElementById("user_about_me").innerText = userData.aboutMe;
+//   document.getElementById("user_handle").innerText = ("@" + userData.username);
+//   document.getElementById("user_name").innerText = userData.fullName;
+// }
+
+
+  $(document).ready(() => {
+    $.get("/profile-data", (data) => {
+      // contains array of profile documents from the server
+      const profile = data[0]; // assumes only 1 profile in collection
+      
+      // banner updated
+      //$("#banner_image").attr("src", profile.bannerImageUrl);
+      
+      // profile pic updated
+      $("#profile_pic").attr("src", profile.profilePicture);
+      
+      // name and handle updated
+      $(".text").html(`${profile.fullName} <br> @${profile.username}`);
+      
+      // about me section updated
+      $(".about-me").html(profile.aboutMe);
+
+    });
+  });
 
 
 // to change image for banner when user clicks the banner
