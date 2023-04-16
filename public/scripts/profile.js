a5add4d64540059cfdf895f9cef99484d15d785c
SaraBZL <sara.aziz.bhatti@gmail.com>
2023-04-16 18:19:48 -0400

    error fix
    fixed the addeventlistener error so that the change banner and image works

--- public/scripts/profile.js
+++ public/scripts/profile.js
@@ -23,55 +23,53 @@
 // }
 
 
-  $(document).ready(() => {
-    $.get("/profile-data", (data) => {
-      // contains array of profile documents from the server
-      const profile = data[0]; // assumes only 1 profile in collection
-      
-      // banner updated
-      //$("#banner_image").attr("src", profile.bannerImageUrl);
-      
-      // profile pic updated
-      $("#profile_pic").attr("src", profile.profilePicture);
-      
-      // name and handle updated
-      $(".text").html(`${profile.fullName} <br> @${profile.username}`);
-      
-      // about me section updated
-      $(".about-me").html(profile.aboutMe);
+$(document).ready(() => {
+  $.get("/profile-data", (data) => {
+    // contains array of profile documents from the server
+    const profile = data[0]; // assumes only 1 profile in collection
+    
+    // banner updated
+    //$("#banner_image").attr("src", profile.bannerImageUrl);
+    
+    // profile pic updated
+    $("#profile_pic").attr("src", profile.profilePicture);
+    
+    // name and handle updated
+    $(".text").html(`${profile.fullName} <br> @${profile.username}`);
+    
+    // about me section updated
+    $(".about-me").html(profile.aboutMe);
 
+    //to change image for banner when user clicks the banner
+    $("#banner_image").on("click", function() {
+      $("#banner_input").click();
+    });
+
+    $("#banner_input").on("change", function() {
+      const banner = this.files[0];
+      const fileReader = new FileReader();
+      
+      fileReader.onload = function(event) {
+        $("#banner_image").attr("src", event.target.result);
+      };
+  
+      fileReader.readAsDataURL(banner);
+    });
+
+    //to change image for profile pic when user clicks the image
+    $("#profile_pic").on("click", function() {
+      $("#profile_input").click();
+    });
+
+    $("#profile_input").on("change", function() {
+      const profile = this.files[0];
+      const fileReader = new FileReader();
+      
+      fileReader.onload = function(event) {
+        $("#profile_pic").attr("src", event.target.result);
+      };
+  
+      fileReader.readAsDataURL(profile);
     });
   });
-
-
-// to change image for banner when user clicks the banner
-document.getElementById("banner_image").addEventListener("click", function() {
-  document.getElementById("banner_input").click();
-});
-
-document.getElementById("banner_input").addEventListener("change", function() {
-  const banner = this.files[0];
-  const fileReader = new FileReader();
-  
-  fileReader.onload = function(event) {
-    document.getElementById("banner_image").src = event.target.result;
-  };
-
-  fileReader.readAsDataURL(banner);
-});
-
-//to change image for profile pic when user clicks the image
-document.getElementById("profile_pic").addEventListener("click", function() {
-  document.getElementById("profile_input").click();
-});
-
-document.getElementById("profile_input").addEventListener("change", function() {
-  const profile = this.files[0];
-  const fileReader = new FileReader();
-  
-  fileReader.onload = function(event) {
-    document.getElementById("profile_pic").src = event.target.result;
-  };
-
-  fileReader.readAsDataURL(profile);
 });
