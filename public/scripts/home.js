c70df61aca03c92bcba5994d6f209987f499dd77
Thomas Chiarello <78321241+GlazedSyrup@users.noreply.github.com>
2023-04-16 00:03:18 -0400

    Added functional dynamic posts
    potential bug in the home css file? bug causes some offset in graphics including the nav bar(which was untouched), figuring out the problem.

--- public/scripts/home.js
+++ public/scripts/home.js
@@ -1,52 +1,6 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const parent = document.querySelector(".page");
 
-    fetch("https://jsonplaceholder.typicode.com/posts")
-        .then(response => response.json())
-        .then(data => {
-            for (let i = 0; i < data.length; i++) {
-                const value = data[i];
-                const section = document.createElement('section');
-                section.classList.add("post", "hidden");
-
-                const header = document.createElement('div');
-                header.classList.add("header");
-
-                const img = document.createElement("img");
-                img.src = "images/default.jpg";
-                header.appendChild(img);
-
-                const h2 = document.createElement("h2");
-                h2.textContent = value.title;
-                header.appendChild(h2);
-
-                section.appendChild(header);
-
-                const content = document.createElement("div");
-                content.classList.add("content");
-
-                const p = document.createElement("p");
-                p.textContent = value.body;
-                content.appendChild(p);
-
-                section.appendChild(content);
-
-                if (parent) {
-                    parent.appendChild(section);
-                } else {
-                    console.log("Parent element not found.");
-                };
-            }
-        })
-        .catch(error => console.log(error));
-
-
-});
-
-document.addEventListener('DOMContentLoaded', function() {
 // This code was adapted from "app.js" by Beyond Fireship (2023-04-05)
-// let page = $("div.page").children();
-const observer = new IntersectionObserver((entries => {
+const observer = new IntersectionObserver(entries => {
     entries.forEach((entry) => {
         console.log(entry)
         if(entry.isIntersecting){
@@ -55,8 +9,56 @@ const observer = new IntersectionObserver((entries => {
             entry.target.classList.remove('show');
         }
     });
-}));
+});
 
-const hiddenElements = document.querySelectorAll('.hidden');
-hiddenElements.forEach((element) => observer.observe(element));
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    const parent = document.querySelector(".page");
+
+    fetch("https://jsonplaceholder.typicode.com/posts")
+        .then(response => response.json())
+        .then(data => {
+            let number_of_elements = 0;
+
+            for (let i = 0; i < data.length; i++) {
+                let  value = data[i];
+                let  section = document.createElement('section');
+                section.classList.add("post", "hidden");
+
+                let  header = document.createElement('div');
+                header.classList.add("header");
+
+                let  img = document.createElement("img");
+                img.src = "images/default.jpg";
+                header.appendChild(img);
+
+                let  h2 = document.createElement("h2");
+                h2.textContent = value.title;
+                header.appendChild(h2);
+
+                section.appendChild(header);
+
+                let  content = document.createElement("div");
+                content.classList.add("content");
+
+                let  p = document.createElement("p");
+                p.textContent = value.body;
+                content.appendChild(p);
+
+                section.appendChild(content);
+
+                if (parent) {
+                    number_of_elements++;
+                    console.log(number_of_elements)
+                    parent.appendChild(section);
+                    observer.observe(section)
+                } else {
+                    console.log("Parent element not found.");
+                };
+                //console.log(number_of_elements)
+            }
+        })
+        .catch(error => console.log(error));
+
 });
