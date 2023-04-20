1b032077afcd620d5ef82b2ec3bc2ced88bcae56
SaraBZL <sara.aziz.bhatti@gmail.com>
2023-04-19 21:34:10 -0400

    url upload
    added url upload

--- public/scripts/profile.js
+++ public/scripts/profile.js
@@ -23,39 +23,42 @@
 // }
 
 
-window.onload = function() {
-  // to change image for banner when user clicks the banner
-  document.getElementById("banner_image").addEventListener("click", function() {
-    document.getElementById("banner_input").click();
-  });
 
-  document.getElementById("banner_input").addEventListener("change", function() {
-    const banner = this.files[0];
-    const fileReader = new FileReader();
+
+
+// window.onload = function() {
+//   // to change image for banner when user clicks the banner
+//   document.getElementById("banner_image").addEventListener("click", function() {
+//     document.getElementById("banner_input").click();
+//   });
+
+//   document.getElementById("banner_input").addEventListener("change", function() {
+//     const banner = this.files[0];
+//     const fileReader = new FileReader();
     
-    fileReader.onload = function(event) {
-      document.getElementById("banner_image").src = event.target.result;
-    };
+//     fileReader.onload = function(event) {
+//       document.getElementById("banner_image").src = event.target.result;
+//     };
 
-    fileReader.readAsDataURL(banner);
-  });
+//     fileReader.readAsDataURL(banner);
+//   });
 
-  //to change image for profile pic when user clicks the image
-  document.getElementById("profile_pic").addEventListener("click", function() {
-    document.getElementById("profile_input").click();
-  });
+//   //to change image for profile pic when user clicks the image
+//   document.getElementById("profile_pic").addEventListener("click", function() {
+//     document.getElementById("profile_input").click();
+//   });
 
-  document.getElementById("profile_input").addEventListener("change", function() {
-    const profile = this.files[0];
-    const fileReader = new FileReader();
+//   document.getElementById("profile_input").addEventListener("change", function() {
+//     const profile = this.files[0];
+//     const fileReader = new FileReader();
     
-    fileReader.onload = function(event) {
-      document.getElementById("profile_pic").src = event.target.result;
-    };
+//     fileReader.onload = function(event) {
+//       document.getElementById("profile_pic").src = event.target.result;
+//     };
 
-    fileReader.readAsDataURL(profile);
-  });
-};
+//     fileReader.readAsDataURL(profile);
+//   });
+// };
 
 
 
@@ -153,3 +156,12 @@ function cancelPost() {
 
 
 
+document.addEventListener('DOMContentLoaded', () => {
+  document.querySelector('#banner_image_url').addEventListener('input', () => {
+    document.querySelector('#banner_image').src = document.querySelector('#banner_image_url').value;
+  });
+
+  document.querySelector('#profile_pic_url').addEventListener('input', () => {
+    document.querySelector('#profile_pic').src = document.querySelector('#profile_pic_url').value;
+  });
+});
